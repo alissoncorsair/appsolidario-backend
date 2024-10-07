67c4f02e0b9d937adcf6c1fd8f7e10cad5c1ac47
alisson <alissoncorsair@hotmail.com>
2024-10-07 20:35:33 -0300

    remove title

--- types/types.go
+++ types/types.go
@@ -142,7 +142,6 @@ type Post struct {
 }
 
 type CreatePostRequest struct {
-	Title       string `json:"title" validate:"required,max=255"`
 	Description string `json:"description" validate:"required"`
 }
 
